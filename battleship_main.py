b2ef70ada8410fd46cbbac10bce5e41b7c08cf6d
Rohan Sharan <sharanrohan@berkeley.edu>
2016-07-21 15:16:54 -0400

    got computer placing ships

--- battleship_main.py
+++ battleship_main.py
@@ -1,8 +1,10 @@
-from random import randint
+import random
 from graphics import *
 
 allsquares = []
-allships = []
+all_user_ships = []
+all_computer_ships = []
+
 
 class GridPoint:
     def __init__(self, x, y, size):
@@ -51,8 +53,7 @@ class Ship:
         self.increment = increment
         self.allPoints = []
         for i in range(length):
-            self.allPoints.append((coord1[0] + increment[0] * i, coord1[1] + increment[1] * i))
-        print(self.allPoints)
+            self.allPoints.append((self.coord1[0] + increment[0] * i, self.coord1[1] + increment[1] * i))
 
     def getPoints(self):
         return self.allPoints
@@ -63,6 +64,21 @@ class Ship:
             if point in other_points:
                 return True
         return False
+
+    def draw(self, graphics_window):
+        if self.increment == (1, 0) or self.increment == (0, 1):
+            p1 = Point(2 * self.coord1[0], 23 - 2 * self.coord1[1])
+            p2 = Point(2 + 2 * self.coord2[0], 21 - 2 * self.coord2[1])
+        elif self.increment == (-1, 0):
+            p1 = Point(2 + 2 * self.coord1[0], 23 - 2 * self.coord1[1])
+            p2 = Point(2 * self.coord2[0], 21 - 2 * self.coord2[1])
+        elif self.increment == (0, -1):
+            p1 = Point(2 + 2 * self.coord1[0], 21 - 2 * self.coord1[1])
+            p2 = Point(2 * self.coord2[0], 23 - 2 * self.coord2[1])
+
+        o = Oval(p1, p2)
+        o.setFill("black")
+        o.draw(graphics_window)
 
 
 
@@ -125,22 +141,27 @@ def placeUserShips(window):
 
     ship5.draw(window)
     s5 = placeShip(window, 5, None)
+    s5.draw(window)
     ship5.undraw()
 
     ship4.draw(window)    
     s4 = placeShip(window, 4, [s5])
+    s4.draw(window)
     ship4.undraw()    
 
     ship3.draw(window)    
     s3_1 = placeShip(window, 3, [s5, s4])
+    s3_1.draw(window)
     ship3.undraw()    
 
     ship32.draw(window)    
     s3_2 = placeShip(window, 3, [s5, s4, s3_1])
+    s3_2.draw(window)
     ship32.undraw()    
 
     ship2.draw(window)    
     s2 = placeShip(window, 2, [s5, s4, s3_1, s3_2])
+    s2.draw(window)
     ship2.undraw()    
 
     
@@ -149,16 +170,22 @@ def placeUserShips(window):
 def placeShip(window, length, other_ships):
     while True:
         correct_len = False
+        overlapping = False
         p = window.getMouse()
         for square in allsquares:
             if square.within(p):
-                square.getRectangle().setOutline('red')
                 break
+        if (2 > p.getX()) or (22 < p.getX()) or (1 > p.getY()) or (21 < p.getY()):
+            print("You clicked out of the ship placement area. Please try again.")
+            continue
         p2 = window.getMouse()
         for square2 in allsquares:
             if square2.within(p2):
-                square2.getRectangle().setOutline('green')
                 break
+        if (2 > p2.getX()) or (22 < p2.getX()) or (1 > p2.getY()) or (21 < p2.getY()):
+            print("You clicked out of the ship placement area. Please try again.")
+            continue
+
         if (square.getCoords()[0] + length - 1) == (square2.getCoords()[0]) and (square.getCoords()[1]) == (square2.getCoords()[1]):
             correct_len = True
             ship =  Ship(length, square.getCoords(), square2.getCoords(), (1, 0))
@@ -172,20 +199,55 @@ def placeShip(window, length, other_ships):
             correct_len = True
             ship = Ship(length, square.getCoords(), square2.getCoords(), (0, -1))
         if correct_len == False:
-            print("The ship is not the correct length")
-            square.getRectangle().setOutline('black')
-            square2.getRectangle().setOutline('black')
+            print("The ship is not the correct length. Please try again.")
             continue
         if other_ships is not None:
             for other in other_ships:
                 if other.checkOverlap(ship):
-                    print("Overlapping ship, please try placing th ship again!")
-                    square.getRectangle().setOutline('black')
-                    square2.getRectangle().setOutline('black')
-                    continue
-        return ship
+                    print("You tried to place your ship on top of another one, please try placing the ship again!")
+                    overlapping = True
+                    break
+        if correct_len == True and overlapping == False:
+            all_user_ships.append(ship)    
+            return ship
 
 
+def placeComputerShips(window):
+    comp5 = placeComputerShip(5, None)
+    comp5.draw(window)
+
+    comp4 = placeComputerShip(4, [comp5])
+    comp4.draw(window)
+
+    comp3_1 = placeComputerShip(3, [comp5, comp4])
+    comp3_1.draw(window)
+
+    comp3_2 = placeComputerShip(3, [comp5, comp4, comp3_1])
+    comp3_2.draw(window)
+
+    comp2 = placeComputerShip(2, [comp5, comp4, comp3_1, comp3_2])
+    comp2.draw(window)
+
+
+def placeComputerShip(length, other_ships):
+    while True:
+        overlapping = False
+        while True:
+            anchor_point = random.choice(allsquares)
+            directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+            direction = random.choice(directions)
+            end_point = (anchor_point.getCoords()[0] + direction[0] * (length - 1), anchor_point.getCoords()[1] + direction[1] * (length - 1))
+            if (1 <= end_point[0] <= 10) and (1 <= end_point[1] <= 10):
+                break
+
+        ship = Ship(length, anchor_point.getCoords(), end_point, direction)
+        if other_ships is not None:
+            for other in other_ships:
+                if other.checkOverlap(ship):
+                    overlapping = True
+                    break
+        if overlapping == False:
+            return ship
 
 
 
@@ -197,12 +259,18 @@ def main():
         else:
             print('Your size must be between 150 and 550! Please try again.\n')
 
-    window = GraphWin("BattleShip", size * 141 / 100.0, size * 131 / 100.0)
+    window = GraphWin("User BattleShip", size * 141 / 100.0, size * 131 / 100.0)
     window.setCoords(0, 0, 28, 28)
     drawBoard(size, window)
 
-    check = placeUserShips(window)
-    print(check)
+    placeUserShips(window)
+
+    window2 = GraphWin("Computer BattleShip", size * 121 / 100.0, size * 111 / 100.0)
+    window2.setCoords(0, 0, 23, 23)
+    drawBoard(size, window2)
+
+    placeComputerShips(window2)
+
 
     window.getMouse()
     window.close()
