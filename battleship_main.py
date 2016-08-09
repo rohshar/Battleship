fab1579cb7bff479870307a9829b8b75cc715610
Rohan Sharan <sharanrohan@berkeley.edu>
2016-08-08 21:18:24 -0700

    added more gui functionality

--- battleship_main.py
+++ battleship_main.py
@@ -1,41 +1,62 @@
 from drawables import GridPoint, GridSquare, Ship
 from players import HumanPlayer, ComputerPlayer
+from intro_gui_utils import start, instruct, SampleApp
 from graphics import *
-
 
 def main():
     """
     Contains the logic that gets user input and executes the actual BattleShip game.
     """
     while True:
-        size = input('How large do you want the BattleShip window to be? (from 150-550 px) ')
-        if (150 <= size <= 550):
+        choice = input('Text input interface (1) or GUI input interface (2)? ')
+        if (choice == 1) or (choice == 2):
             break
         else:
-            print('Your size must be between 150 and 550! Please try again.\n')
+            print('You much choose 1 or 2! Please try again.\n')
 
-    while True:
-        option = input('Do you wanna play: player vs. player (1), player vs. CPU (2), CPU vs. CPU (3) ')
-        if (option == 1) or (option == 2) or (option == 3):
+
+    if (choice == 1):
+        while True:
+            size = input('How large do you want the BattleShip window to be? (from 150-550 px) ')
+            if (150 <= size <= 550):
+                break
+            else:
+                print('Your size must be between 150 and 550! Please try again.\n')
+
+        while True:
+            option = input('Do you wanna play: player vs. player (1), player vs. CPU (2), CPU vs. CPU (3) ')
+            if (option == 1) or (option == 2) or (option == 3):
+                break
+            else:
+                print('You must choose an appropriate option!\n')
+
+        while True:
+            if option == 2:
+                diff = input('What CPU level do you want? (1: very easy, 2: easy, 3: medium, 4: hard, 5: very hard) ')
+                if (1 <= diff <= 5):
+                    break
+                else:
+                    print('You must choose an appropriate difficulty!\n')
+            if option == 3:
+                diff = input('What CPU level do you want for the first CPU? (1: very easy, 2: easy, 3: medium, 4: hard, 5: very hard) ')
+                diff2 = input('What CPU level do you want for the second CPU? (1: very easy, 2: easy, 3: medium, 4: hard, 5: very hard) ')
+                if (1 <= diff <= 5) and (1 <= diff2 <= 5):
+                    break
+                else:
+                    print('You must choose appropriate difficulties!\n')
             break
+    else:
+        check = start()
+        if check:
+            app = SampleApp()
+            app.mainloop()
+            size = app.size
+            option = app.option
+            diff = app.diff
+            diff2 = 5
         else:
-            print('You must choose an appropriate option!\n')
-
-    while True:
-        if option == 2:
-            diff = input('What CPU level do you want? (1: very easy, 2: easy, 3: medium, 4: hard, 5: very hard) ')
-            if (1 <= diff <= 5):
-                break
-            else:
-                print('You must choose an appropriate difficulty!\n')
-        if option == 3:
-            diff = input('What CPU level do you want for the first CPU? (1: very easy, 2: easy, 3: medium, 4: hard, 5: very hard) ')
-            diff2 = input('What CPU level do you want for the second CPU? (1: very easy, 2: easy, 3: medium, 4: hard, 5: very hard) ')
-            if (1 <= diff <= 5) and (1 <= diff2 <= 5):
-                break
-            else:
-                print('You must choose appropriate difficulties!\n')
-        break
+            instruct()
+            main()
 
     if option == 1:
         player1 = HumanPlayer()
@@ -47,12 +68,12 @@ def main():
         player1 = ComputerPlayer(diff)
         player2 = ComputerPlayer(diff2)
 
-    window = GraphWin("User BattleShip", size * 141 / 100.0, size * 131 / 100.0)
+    window = GraphWin("Player 1 BattleShip", size * 141 / 100.0, size * 131 / 100.0)
     window.setCoords(0, 0, 28, 28)
     player1.drawBoard(window)
     player1.placeShips(window)
 
-    window2 = GraphWin("Computer BattleShip", size * 121 / 100.0, size * 111 / 100.0)
+    window2 = GraphWin("Player 2 BattleShip", size * 121 / 100.0, size * 111 / 100.0)
     window2.setCoords(0, 0, 23, 23)
     player2.drawBoard(window2)
     player2.placeShips(window2)
---
+++ intro_gui_utils.py
@@ -0,0 +1,114 @@
+from graphics import *
+import tkinter as tk
+
+
+class SampleApp(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.wm_title("Input Stage")
+        self.label1 = tk.Label(self, text='How large do you want the BattleShip window to be? (from 150-550 px)')
+        self.label1.pack()
+        self.entry1 = tk.Entry(self)
+        self.entry1.pack()
+
+        self.label2 = tk.Label(self, text='Do you wanna play: player vs. player (1), player vs. CPU (2), CPU vs. CPU (3)')
+        self.label2.pack()
+        self.entry2 = tk.Entry(self)
+        self.entry2.pack()
+
+        self.label3 = tk.Label(self, text='What CPU level do you want? (1: very easy, 2: easy, 3: medium, 4: hard, 5: very hard)')
+        self.label3.pack()
+        self.entry3 = tk.Entry(self)
+        self.entry3.pack()
+
+        self.button = tk.Button(self, text="Start playing", command=self.on_button)
+        self.button.pack()
+
+    def on_button(self):
+        size = int(self.entry1.get())
+        option = int(self.entry2.get())
+        diff = None
+        if self.entry2.get():
+            diff = int(self.entry3.get())
+        if (150 <= size <= 550):
+            if (option == 1) or (option == 2) or (option == 3):
+                if (option == 2) or (option == 3):
+                    if (1 <= diff <= 5):
+                        self.destroy()
+                        self.size = size
+                        self.option = option
+                        self.diff = diff
+                        self.quit()
+                elif (option == 1):
+                    self.destroy()
+                    self.size = size
+                    self.option = option
+                    self.diff = diff
+                    self.quit()
+
+        print("You must input appropriate choices!")
+        self.destroy()
+        app = SampleApp()
+        app.mainloop()
+
+
+def instruct():
+    helper=GraphWin("Instructions", 500, 500)
+    helper.setBackground('Pink')
+    helper.setCoords(0, 0, 10, 10)
+    instructions=Text(Point(5, 5), "Welcome to Battleship!\n \nIn this game, "
+                      "you will pick points on\n a 10x10 grid to choose where"
+                      " your five\n ships will be placed. You will have one \n"
+                      "ship of length five, one ship of \nlength four, two ships "
+                      "of length three, and\n one ship of length two. Then, you\n"
+                      "will proceed to choose points on the\n computer's grid and"
+                      " check whether the shot\n was a hit or a miss. A red dot is a\n"
+                      "hit and a white dot is a miss. The computer\n"
+                      "will then do the same. When all \n"
+                      "spaces for a given ship are hit,\n that ship sinks. When"
+                      " a player sinks all\n of his or her opponent's ships, the \n"
+                      "player wins. \n\n Click the window to go back!")
+    instructions.setSize(20)
+    instructions.draw(helper)
+    helper.getMouse()
+    helper.close()
+
+def start():
+
+    p = Point(0, 0)
+    img = Image(p, "battle1.gif")
+
+    width = img.getWidth()
+    height = img.getHeight()
+
+    img.move(width/2,height/2)
+
+    intro=GraphWin("Intro Page", width, height)
+    img.draw(intro)
+    btext=Text(Point(250, 75), "Battleship")
+    btext.setSize(20)
+    btext.setStyle('bold italic')
+    btext.draw(intro)
+    srec=Rectangle(Point(200,150), Point(300, 200))
+    srec.setFill("coral")
+    srec.draw(intro)
+    stext=Text(Point(250, 175), "Start")
+    stext.setFill("black")
+    stext.draw(intro)
+    irec=Rectangle(Point(200, 250), Point(300, 300))
+    irec.setFill("coral")
+    irec.draw(intro)
+    itext=Text(Point(250, 275), "Instructions")
+    itext.setFill("black")
+    itext.draw(intro)
+
+    while True:
+        p=intro.getMouse()
+        x=p.getX()
+        y=p.getY()
+        if 200<x<300 and 150<y<200:
+            intro.close()
+            return True
+        if 200<x<300 and 250<y<300:
+            intro.close()
+            return False
